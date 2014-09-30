459e1bd44543272ec23978634a5599f26afa5f5e
ragden <epaz@CPU165.local>
2014-09-30 17:59:41 +0200

    Added json example and storyboard for sample.

---
+++ ILJSONDataExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ILJSONDataExample
+//
+//  Created by Edgar Paz on 30/09/14.
+//  Copyright (c) 2014 Edgar Paz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ILJSONDataExampleTests/ILJSONDataExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  ILJSONDataExampleTests.m
+//  ILJSONDataExampleTests
+//
+//  Created by Edgar Paz on 30/09/14.
+//  Copyright (c) 2014 Edgar Paz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ILJSONDataExampleTests : XCTestCase
+
+@end
+
+@implementation ILJSONDataExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
